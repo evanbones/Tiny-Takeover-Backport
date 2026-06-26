7c66b3101b44b34adaf7b9dd0c6f88c767d14316
evanbones <74276616+evanbones@users.noreply.github.com>
2026-06-26 12:51:53 -0700

    fix z-fighting

--- common/src/main/java/com/evandev/tiny_takeover_backport/client/model/BabyAxolotlModel.java
+++ common/src/main/java/com/evandev/tiny_takeover_backport/client/model/BabyAxolotlModel.java
@@ -71,7 +71,7 @@ public class BabyAxolotlModel extends AxolotlModel {
         head.addOrReplaceChild(
                 "top_gills",
                 CubeListBuilder.create().texOffs(20, 0).addBox(-3.0F, -3.0F, 0.0F, 6.0F, 3.0F, 0.0F, new CubeDeformation(0.0F)),
-                PartPose.offset(0.0F, -2.0F, -2.0F)
+                PartPose.offset(0.0F, -2.0F, -2.01F)
         );
         return LayerDefinition.create(meshdefinition, 32, 32);
     }
